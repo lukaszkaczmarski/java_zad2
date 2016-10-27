38ef73855848847204173fcd1aacad7e788ea987
Łukasz Kaczmarski <lukasz.kaczmarski93@gmail.com>
2016-10-27 16:45:36 +0200

    hello wordl

--- Zad2/src/main/java/com/lukaszkaczmarski/Zad2Application.java
+++ Zad2/src/main/java/com/lukaszkaczmarski/Zad2Application.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Zad2Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		SpringApplication.run(Zad2Application.class, args);
+		System.out.println("Hello World");
 	}
 }
